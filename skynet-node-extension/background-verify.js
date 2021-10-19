3f68de415e1ee27c8dc59ad01567c06178d1c953
David Vorick <david@nebulous.tech>
2021-10-19 02:57:51 -0400

    set response headers properly on node.siasky.net

--- skynet-node-extension/background-verify.js
+++ skynet-node-extension/background-verify.js
@@ -1,17 +1,34 @@
 // Create a listener that completely swallows the pages of the given URLs,
 // returning nothing instead.
 //
-// TODO: Ideally instead of just replacing the request with nothing, the
-// request is never fired in the first place, accomplishing two things. First,
-// the round trip time to the network is eliminated. Second, the user does not
-// need siasky.net to be online for them to access Skynet.
+// TODO: Ideally, instead of just replacing the request with nothing and
+// resetting the headers, we use redirectUrl to load an extension page that has
+// all of the code that we want. I couldn't figure out how to get it working
+// though, all attempts resulted in the extension not running.
 function listener(details) {
   let filter = browser.webRequest.filterResponseData(details.requestId);
   filter.onstop = event => {
     filter.disconnect();
   }
-
   return {};
+
+  // return {redirectUrl: "https://google.com"}; - this works
+  // return {redirectUrl: "homescreen.html"}; - this doesn't work
+  // return {cancel: true}; - this cancels the request, but I don't know how to serve a webpage afterwards
+}
+
+// Since we can't seem to cancel the request entirely, we need to swallow the
+// response headers and replace them with custom responses. We don't want the
+// portal interfering with the operation of our kernel by adding malicious
+// response headers.
+function setResponse(details) {
+  let newHeaders = [
+    {
+      name: "content-type",
+      value: "text/html; charset=utf8"
+    }
+  ]
+  return {responseHeaders: newHeaders};
 }
 
 // The extension will listen to all requests that go to node.siasky.net and
@@ -28,4 +45,10 @@ browser.webRequest.onBeforeRequest.addListener(
   listener,
   {urls: ["https://node.siasky.net/*", "https://home.siasky.net/*"]},
   ["blocking"]
+);
+
+browser.webRequest.onHeadersReceived.addListener(
+  setResponse,
+  {urls: ["https://node.siasky.net/*", "https://home.siasky.net/*"]},
+  ["blocking", "responseHeaders"]
 );
