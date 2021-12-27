69403621ed19776371ab4a5139301e00bd12704b
David Vorick <david@siasky.net>
2021-12-27 13:40:18 -0500

    begin messing around with the registry

--- skynet-kernel-extension/src/content-kernel.ts
+++ skynet-kernel-extension/src/content-kernel.ts
@@ -5,6 +5,17 @@ export {};
 // have the kernel operate entirely from shared workers. Still need to explore
 // that.
 
+// TODO: The next step is that we need to actually set up the flow for figuring
+// out the user's preferred portal, right? Or do we keep trucking with the v1
+// downloads? At some point we need to have the user set up a portal, I guess
+// that can happen later?
+
+// TODO: First we get the user's preferred portal, then we get the user's
+// preferred kernel. Then instead of loading the default kernel we load the
+// user's preferred kernel. If the user doesn't have a preferred kernel, we can
+// set the user's preferred kernel. So that's where we can get the first
+// writeRegistry item in place.
+
 // Set a title and a message which indicates that the page should only be
 // accessed via an invisible iframe.
 console.log("progress", "kernel has been opened");
@@ -16,6 +27,8 @@ document.body.appendChild(header);
 // import:::skynet-kernel-extension/lib/sha512.ts
 
 // import:::skynet-kernel-extension/lib/ed25519.ts
+
+var defaultPortalList = ["siasky.net"];
 
 // getUserSeed will return the seed that is stored in localStorage. This is the
 // first function that gets called when the kernel iframe is openend. The
@@ -43,6 +56,108 @@ var logOut = function() {
 	localStorage.clear();
 }
 
+var buf2hex = function(buffer: ArrayBuffer) { // buffer is an ArrayBuffer
+	return [...new Uint8Array(buffer)]
+		.map(x => x.toString(16).padStart(2, '0'))
+		.join('');
+}
+
+// readOwnRegistryEntry will read and verify a registry entry that is owned by
+// the user. The tag strings will be hashed with the user's seed to produce the
+// correct entropy.
+// 
+// TODO: This function currently protects the user from a portal providing
+// incorrect data, but does not protect the user from a portal providing
+// outdated data or a false 404. Getting protection against those would require
+// receiving a list of signatures from a set of hosts (likely established TOFU
+// style through previous successful lookups) to verify that multiple trusted
+// hosts as well as the portal are reporting the same result.
+//
+// TODO: Need to provide error and success callbacks, as this is a generic
+// function. Or maybe we just provide response callbacks? Let the caller decide
+// how to handle the response? Probably for the better that way.
+var readOwnRegistryEntry = function(keyPairTagStr: string, dataKeyTagStr: string, callback: any) {
+	// Use the user's seed to derive the registry entry that is going to contain
+	// the user's portal list.
+	let keyPairEntropy = new Uint8Array(HASH_SIZE);
+	let keyPairTag = new TextEncoder().encode(keyPairTagStr);
+	let entropyInput = new Uint8Array(keyPairTag.length+userSeed.length);
+	entropyInput.set(keyPairTag);
+	entropyInput.set(userSeed, keyPairTag.length);
+	sha512(keyPairEntropy, entropyInput, entropyInput.length);
+	// Use the user's seed to dervie the dataKey for the registry entry. We use
+	// a different tag to ensure that the dataKey is independently random, such
+	// that the registry entry looks like it could be any other registry entry.
+	let dataKeyEntropy = new Uint8Array(HASH_SIZE);
+	let dataKeyTag = new TextEncoder().encode(dataKeyTagStr);
+	let dataKeyInput = new Uint8Array(dataKeyTag.length+userSeed.length);
+	dataKeyInput.set(dataKeyTag);
+	dataKeyInput.set(userSeed, dataKeyTag.length);
+	sha512(dataKeyEntropy, dataKeyInput, dataKeyInput.length);
+
+	// Create the private key for the registry entry.
+	let keyPair = keyPairFromSeed(keyPairEntropy.slice(0, 32));
+	let dataKey = dataKeyEntropy.slice(0, 32);
+
+	// Try signing and verifying some data using the keyPair.
+	// 
+	// TODO: This only needs to be here for as long as we aren't doing the
+	// full registry fetch and verify.
+	let message = new TextEncoder().encode("this is a test message to sign");
+	let message8 = new Uint8Array(message.length);
+	message8.set(message);
+	try {
+		let sig = sign(message8, keyPair.secretKey);
+		let result = verify(message8, sig, keyPair.publicKey);
+		if (!result) {
+			console.log("error", "pubkey verification failed", err);
+			throw 'pubkey verification failed when trying to get the list of portals from the registry';
+		}
+	} catch(err) {
+		console.log("error", "unable to produce signature from keypair", err);
+	}
+
+	// Get a list of portals, then try fetching the entry from each portal
+	// until a successful response is received. A 404 is considered a
+	// successful response.
+	// 
+	// TODO: Need to check whether we need to add the 'hashedDataKeyHex'
+	// param to our query strings. The docs say the default is 'false'
+	// (meaning the portal will hash it for us), but I think the default is
+	// actually 'true' (meaning the portal assumes it is already hashed).
+	// The example doesn't use it and provides an already hashed datakey.
+	// 
+	// TODO: The actual way that we iterate through the portals is going to
+	// be complicated. Not likely to be a for loop.
+	let portalList = preferredPortals();
+	for (let i = 0; i < portalList.length; i++) {
+		let portal = portalList[i];
+		let pubkeyHex = buf2hex(keyPair.publicKey);
+		let dataKeyHex = buf2hex(dataKey);
+		let query = "https://" + portal + "/skynet/registry?publickey=ed25519%3A"+pubkeyHex+"&datakey="+dataKeyHex;
+		console.log("registryGET", query);
+		callback()
+		fetch(query)
+			.then(response => {
+				// TODO: If the status is not 404, we need to
+				// parse and verify the entry and then use the
+				// contents of that entry to set the
+				// localstorage list of portals. This is being
+				// postponed for the moment because we don't
+				// have any flow to write the user's preferred
+				// list of portals.
+				console.log("RESPONSE HAS BEEN RECEIVED");
+				console.log(response);
+				console.log(response.status);
+			})
+		return
+	}
+
+	// Fetch the registry data.
+	// 
+	// TODO: Design the error handling.
+}
+
 // downloadV1Skylink will download the raw data for a skylink and then verify
 // that the downloaded content matches the hash of the skylink.
 // 
@@ -65,8 +180,17 @@ var downloadV1Skylink = function(skylink: string) {
 // access the Sia registry using a portal list hardcoded by the extension. This
 // should be the only time that the user needs to make a request that is not
 // going directly to their preferred portals.
+// 
+// TODO: To prevent an infinite loop, we need to split the init function off
+// from the core read function. Or maybe the core read function can return...
+// something that will prevent a circular lookup on the registry lookup? This
+// has to return without a network lookup but maybe it can kick off a network
+// lookup? That might cause some spinning still anyway.
+//
+// I think that the best solution here is to have a separate init function
+// which works in the background to fetch the portal list.
 var preferredPortals = function(): string[] {
-	let defaultList = ["siasky.net"];
+	// Try to get the list of portals from localstorage.
 	let portalListStr = window.localStorage.getItem("v1-portalList");
 	if (portalListStr !== null) {
 		try {
@@ -79,70 +203,37 @@ var preferredPortals = function(): string[] {
 			// event that all of the user's portals are offline, the user is
 			// still able to connect to Skynet.
 			let portalList = JSON.parse(portalListStr);
-			return portalList;
+			for (let i = 0; i < defaultPortalList.length; i++) {
+				portalList.basicPortals.push(defaultPortalList[i]);
+			}
+			return portalList.basicPortals;
 		} catch {
+			// TODO: We should probably clear the entry so that the user can make progress.
 			console.log("error", "corrupt portalListStr found in localStorage: "+portalListStr);
 		}
 	}
 
-	// Use the user's seed to derive the registry entry that is going to contain
-	// the user's portal list.
-	let secretKeyEntropy = new Uint8Array(HASH_SIZE);
-	let secretKeyTag = new TextEncoder().encode("v1-skynet-portal-list");
-	let entropyInput = new Uint8Array(secretKeyTag.length+userSeed.length);
-	entropyInput.set(secretKeyTag);
-	entropyInput.set(userSeed, secretKeyTag.length);
-	sha512(secretKeyEntropy, entropyInput, entropyInput.length);
-	// Use the user's seed to dervie the dataKey for the registry entry. We use
-	// a different tag to ensure that the dataKey is independently random, such
-	// that the registry entry looks like it could be any other registry entry.
-	let dataKeyEntropy = new Uint8Array(HASH_SIZE);
-	let dataKeyTag = new TextEncoder().encode("v1-skynet-portal-list-dataKey");
-	let dataKeyInput = new Uint8Array(dataKeyTag.length+userSeed.length);
-	dataKeyInput.set(dataKeyTag);
-	dataKeyInput.set(userSeed, dataKeyTag.length);
-	sha512(dataKeyEntropy, dataKeyInput, dataKeyInput.length);
-
-	// Create the private key for the registry entry.
-	let keyPair = keyPairFromSeed(secretKeyEntropy.slice(0, 32));
-	let dataKey = dataKeyEntropy.slice(0, 32);
-
-	// Try signing and verifying some data using the keyPair.
-	console.log(keyPair);
-	let message = new TextEncoder().encode("this is a test message to sign");
-	let message8 = new Uint8Array(message.length);
-	message8.set(message);
-	try {
-		let sig = sign(message8, keyPair.secretKey);
-		let result = verify(message8, sig, keyPair.publicKey);
-		if (!result) {
-			console.log("error", "pubkey verification failed", err);
-		}
-		console.log("YEEEEEEEEEE");
-		console.log(result);
-	} catch(err) {
-		console.log("error", "unable to produce signature from keypair", err);
-	}
-
-	// TODO: Actually fetch the list from the registry and from Skynet.
-	return defaultList;
+	// No list found. Just provide the default portal list.
+	return defaultPortalList;
 }
 
-// deriveKernelEntry will derive the keypair and tweak for the registry entry
-// that holds the kernel.
-var deriveKernelEntry = function(): [Uint8Array, Uint8Array] {
-	// TODO:
-	//
-	// We can use the sha512 function to derive some unique data, then we
-	// can pass that data to the keyPairFromSeed() function. This will
-	// create a keypair, which we can pull the pubkey from to figure out
-	// what key we are supposed to look up.
-	//
-	// We can use the sha512 function to derive the datakey.
-	//
-	// We can sign the entry using sign when we're pushing an update, and
-	// we can verify the entry using verify.
-	return [null, null]
+// loadUserPortalPreferences will fetch the user's remote portal preferences
+// from their portal registry entry and update local storage to reflect the
+// user's preferences.
+//
+// TODO: Switch this to use registry subscriptions.
+var loadUserPortalPreferences = function() {
+	readOwnRegistryEntry("v1-skynet-portal-list", "v1-skynet-portal-list-dataKey", function() {
+		// TODO: Remove this console.log.
+		console.log("DEBUG", "callback reached");
+	})
+	if (err !== null) {
+		// TODO: Error handling.
+		return;
+	}
+
+	// TODO: decode the registry entry and set localstorage entry for the
+	// portal list.
 }
 
 // loadSkynetKernel handles loading the rest of the skynet-kernel from the user's
@@ -175,6 +266,9 @@ var loadSkynetKernel = function() {
 
 	// TODO: Check localstorage (or perhaps an encrypted indexededdb) for
 	// the kernel to see if it is already loaded.
+
+	// Load the user's preferred portals from remote.
+	loadUserPortalPreferences();
 
 	// TODO: Grab the registry entry that should be holding the location of
 	// the user's kernel.
@@ -284,6 +378,5 @@ if (err !== "") {
 	window.parent.postMessage({kernelMethod: "authFailed"}, "*");
 } else {
 	console.log("progress", "auth succeeded, loading kernel");
-	let portals = preferredPortals();
 	loadSkynetKernel();
 }
