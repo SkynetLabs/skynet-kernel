58d7c5c54a9ae258a3e28f142a0f2c193e9b4bd5
David Vorick <david@siasky.net>
2022-02-15 13:20:44 -0500

    clean up runtime flow in background script

--- skynet-kernel-extension/src/background.ts
+++ skynet-kernel-extension/src/background.ts
@@ -2,44 +2,33 @@ export {}
 
 declare var browser
 
-// TODO: The way that we are providing the kernel responses is insufficient. We
-// can strip the nonce, but everything else should stay. Right now it's more
-// ad-hoc.
-
-// blockUntilKernelLoaded returns a promise that will not resolve until a
-// message is received from the kernel saying that the kernel has loaded.
-var kernelLoadedResolve
-var kernelLoadedPromise = new Promise((resolve, reject) => {
-	kernelLoadedResolve = resolve
-})
+// blockUntilKernelLoaded returns a promise that will resolve when the kernel
+// sends a message indicating it has loaded. We store the resolve function in a
+// global variable so the promise can be resolved by another frame.
+var kernelLoaded
+var kernelLoadedPromise = new Promise(x => { kernelLoaded = x })
 function blockUntilKernelLoaded() {
 	return kernelLoadedPromise
 }
 
-// blockForKernelResponse will wait until the kernel has responded to a
-// particular nonce. The response will be stored in the object with the nonce
-// as the key.
-//
-// TODO: Switch from using setTimeout to using stored promises. Probably want
-// the responses object to contain the promises at the nonce offset, that way
-// you never need to store the full response in the object. I'm not even sure
-// if that's important, but it's clean.
-var responses = new Object()
-function blockForKernelResponse(nonce: number): Promise<Uint8Array> {
+// queryKernel returns a promise that will resolve when the kernel has
+// responded to the query. The resolve function is stored in the kernelQueries
+// object using the nonce as the key. It will be called by the listener that
+// receives the kernel's response.
+var kernelQueriesNonce = 0
+var kernelQueries = new Object()
+function queryKernel(query) {
 	return new Promise(resolve => {
-		if (responses.hasOwnProperty(nonce)) {
-			let resp = responses[nonce]
-			delete responses[nonce]
-			resolve(resp)
-			console.log("resolved\n", nonce, "\n", resp)
-		} else {
-			setTimeout(function() {
-				blockForKernelResponse(nonce)
-				.then(x => {
-				      resolve(x)
-				})
-			}, 20)
-		}
+		blockUntilKernelLoaded()
+		.then(x => {
+			// Grab the next nonce and store a promise resolution in the
+			// kernelQueries object.
+			let nonce = kernelQueriesNonce
+			kernelQueriesNonce++
+			query.nonce = nonce
+			kernelQueries[nonce] = resolve
+			kernelFrame.contentWindow.postMessage(query, "https://kernel.siasky.net")
+		})
 	})
 }
 
@@ -47,61 +36,23 @@ function blockForKernelResponse(nonce: number): Promise<Uint8Array> {
 // scripts. This is largely a passthrough function which sends messages to the
 // kernel, and then relays the responses back to the content script.
 function contentScriptListener(message, sender, sendResponse) {
-	blockUntilKernelLoaded()
-	.then(x => {
-		let nonce = messageNonce
-		messageNonce++
-		message.nonce = nonce
-		kernelFrame.contentWindow.postMessage(message, "https://kernel.siasky.net")
-
-		console.log("blocking for nonce", nonce)
-		blockForKernelResponse(nonce)
-		.then(resp => {
-			console.log("sending a response for nonce", nonce)
-			sendResponse(resp)
-		})
+	queryKernel(message)
+	.then(resp => {
+		sendResponse(resp)
 	})
-	console.log("existing main thread")
 	return true
-}
-
-// getURLFromKernel will ask the kernel for the resource at a given URL,
-// block until the kernel responds, and then return the response provided by
-// the kernel.
-var messageNonce = 0
-function getURLFromKernel(url: string): Promise<Uint8Array> {
-	return new Promise(resolve => {
-		// All requests need to stall until the kernel has loaded.
-		blockUntilKernelLoaded()
-		.then(x => {
-			// Send a mesasge to the kernel asking for the
-			// resource.
-			let nonce = messageNonce
-			messageNonce++
-			kernelFrame.contentWindow.postMessage({
-				kernelMethod: "requestURL",
-				nonce: nonce,
-			}, "https://kernel.siasky.net")
-
-			// Wait for a response from the kernel, and then
-			// resolve with the response.
-			blockForKernelResponse(nonce)
-			.then(resp => {
-				resolve(resp)
-			})
-		})
-	})
 }
 
 // onBeforeRequestListener will handle calls from onBeforeRequest. Calls to the
 // kernel will be swallowed and replaced by a content script. Calls to pages
 // other than the kernel will be passed to the kernel, and the kernel will
 // decide what response is appropriate for the provided call.
+//
+// TODO: Needs work. Only kernel.siasky.net should be excluded, plus maybe an
+// auth page. Need to be able to process all types of requests, not just GET
+// requests.
 function onBeforeRequestListener(details) {
-	// TODO: This should eventually only check for the kernel, allowing all
-	// other calls to pass through. The current architecture hard-codes
-	// several pages in the extension, and we haven't migrated them out
-	// yet.
+	// TODO: Switch to checking === kernel.siasky.net
 	if (details.url !== "https://test.siasky.net/") {
 		let filter = browser.webRequest.filterResponseData(details.requestId)
 		filter.onstart = event => {
@@ -113,7 +64,10 @@ function onBeforeRequestListener(details) {
 	// Ask the kernel what the appropriate response for this URL is.
 	let filter = browser.webRequest.filterResponseData(details.requestId)
 	filter.ondata = event => {
-		getURLFromKernel(details.url)
+		queryKernel({
+			kernelMethod: "requestURL",
+			url: details.url,
+		})
 		.then(response => {
 			filter.write(response)
 			filter.close()
@@ -166,40 +120,35 @@ browser.runtime.onMessage.addListener(contentScriptListener)
 // 'responses' object, and are expected to be deleted by the caller once
 // consumed. The cpu and memory overheads associated with using an object to
 // store responses are unknown.
-//
-// TODO: Need some way to distinguish between responses that need to be added
-// to the response object, and responses that can be directly forwarded to some
-// caller. And I guess we need to know what caller we are forwarding things to.
 window.addEventListener("message", (event) => {
 	// Ignore all messages that aren't coming from the kernel.
 	if (event.origin !== "https://kernel.siasky.net") {
-		console.log("received unwanted message from: ", event.origin)
-		console.log(event)
+		console.log("received unwanted message from: ", event.origin, "\n", event)
 		return
 	}
 
 	if (!("kernelMethod" in event.data)) {
-		console.log("received message without a kernelMethod")
+		console.log("received message without a kernelMethod\n", event.data)
 		return
 	}
 
 	// Listen for the kernel successfully loading.
 	if (event.data.kernelMethod === "skynetKernelLoaded") {
-		console.log("background script kernel has loaded")
-		kernelLoadedResolve()
+		console.log("kernel has loaded")
+		kernelLoaded()
 		return
 	}
 
 	// Check that the response has a nonce.
 	if (!("nonce" in event.data)) {
-		console.log("received a kernel message without a nonce")
+		console.log("received a kernel message without a nonce\n", event.data)
 		return
 	}
 
 	// All other kernel requests are assumed to be associeted with a
 	// response.
-	console.log("setting nonce\n", event.data.nonce, "\n", event.data.response, "\n", event.data)
-	responses[event.data.nonce] = event.data.response
+	kernelQueries[event.data.nonce](event.data.response)
+	delete kernelQueries[event.data.nonce]
 }, false)
 
 // Open an iframe containing the kernel.
