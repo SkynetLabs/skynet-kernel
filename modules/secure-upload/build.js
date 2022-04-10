bb2729d35ddccb4717091f482fca22dae1f73a15
David Vorick <david@siasky.net>
2022-04-10 09:32:13 -0400

    add pw confirm to module build

--- modules/secure-upload/build.js
+++ modules/secure-upload/build.js
@@ -1,8 +1,9 @@
 // This is the standard build script for a kernel module.
 
 fs = require('fs')
-seed = require('./src/build-lib/seed')
 read = require('read')
+seed = require('./src/build-lib/seed')
+reg = require('./src/build-lib/registry')
 
 // Add a newline for readability.
 console.log()
@@ -21,7 +22,7 @@ if (!fs.existsSync('build')) {
 // Determine the seed file.
 let seedFile
 if (process.argv[2] === 'prod') {
-	seedFile = "module-skylink"
+	seedFile = "build/module-double-seed"
 } else if (process.argv[2] === 'dev') {
 	seedFile = "build/dev-seed"
 } else {
@@ -48,8 +49,10 @@ if (process.argv[2] === 'prod') {
 // password reader is async and we can only access the password when using a
 // callback.
 function handlePass(password) {
-	// Create the seed file if it does not exist.
 	try {
+		// If we are running prod and the seed file does not exist, we
+		// need to confirm the password and also warn the user to use a
+		// secure password.
 		if (!(fs.existsSync(seedFile)) && process.argv[2] === 'prod') {
 			// The file does not exist, we need to confirm the
 			// password.
@@ -70,6 +73,10 @@ function handlePass(password) {
 				handlePassConfirm(password)
 			})
 		} else {
+			// If the seed file does exist, or if we are using dev,
+			// there's no need to confirm the password but we do
+			// need to pass the logic off to the handlePassConfirm
+			// callback.
 			handlePassConfirm(password)
 		}
 	} catch(err) {
@@ -82,7 +89,16 @@ function handlePass(password) {
 // has been provided. If not confirmation password is needed, this function
 // will be called anyway using the unconfirmed password as input.
 function handlePassConfirm(password) {
-	// Create the seedFile if it does not exist.
+	// Create the seedFile if it does not exist. For dev we just save the
+	// seed to disk outright, because this is a dev build and therefore not
+	// security sensitive. Also the dev seed does not get pushed to the
+	// github repo.
+	//
+	// For prod, we use the seed to create a new seed (called the shield)
+	// which allows us to verify that the developer has provided the right
+	// password when deploying the module. The shield does get pushed to
+	// the github repo so that the production module is the same on all
+	// devices.
 	if (!(fs.existsSync(seedFile)) && process.argv[2] !== 'prod') {
 		// Generate the seed phrase and write it to the file.
 		let [seedPhrase, errGSP] = seed.generateSeedPhrase(null)
@@ -107,11 +123,8 @@ function handlePassConfirm(password) {
 		// password and write that to disk so that we can publish the
 		// prod seed phrase to the repo without giving anyone the
 		// ability to update the module.
-		//
-		// TODO: Actually we should just compare this against the
-		// prod-module.txt
-		let [seedPhraseShield, errGSP2] = seed.generateSeedPhrase(password)
-		if (errGSP !== null) {
+		let [seedPhraseShield, errGSP2] = seed.generateSeedPhrase(seedPhrase)
+		if (errGSP2 !== null) {
 			console.error("Unable to generate shielded seed phrase: ", errGSP2)
 			process.exit(1)
 		}
@@ -126,13 +139,33 @@ function handlePassConfirm(password) {
 	// with no password.
 	let seedPhrase
 	if (process.argv[2] === 'prod') {
+		// Generate the seed phrase from the password.
 		let [sp, errGSP] = seed.generateSeedPhrase(password)
 		if (errGSP !== null) {
 			console.error("Unable to generate seed phrase: ", errGSP)
 			process.exit(1)
 		}
+		let [seedPhraseShield, errGSP2] = seed.generateSeedPhrase(sp)
+		if (errGSP2 !== null) {
+			console.error("Unable to generate shielded seed phrase: ", errGSP2)
+			process.exit(1)
+		}
+		seedPhraseShieldVerify = fs.readFileSync(seedFile, 'utf8')
+		if (seedPhraseShieldVerify !== seedPhraseShield) {
+			console.error("Incorrect password")
+			process.exit(1)
+		}
+
 		seedPhrase = sp
 	} else {
 		seedPhrase = fs.readFileSync(seedFile, 'utf8')
 	}
+
+	// TODO: Generate the v2 skylink.
+
+	// TODO: For prod, verify the v2 skylink.
+
+	// TODO: Upload the dist file
+
+	// TODO: Update the v2 skylink
 }
