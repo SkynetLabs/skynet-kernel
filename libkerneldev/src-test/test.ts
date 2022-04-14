ef729fec21ff4f24ee63ad7c9a9b4f6f58ad1a32
David Vorick <david@siasky.net>
2022-04-14 08:35:13 -0400

    try to get the first test working

--- libkerneldev/src-test/test.ts
+++ libkerneldev/src-test/test.ts
@@ -1,9 +1,19 @@
 import { generateSeedPhrase } from '../src/seed'
 
-let phraseNullInput = generateSeedPhrase(null)
-console.log(phraseNullInput)
+let failed = false
+function fail(errStr: string, ...inputs: any) {
+	failed = true
+	console.log(new Error(errStr), ...inputs)
+}
 
-console.error("Test suite is not written.")
-process.exit(1)
+let phraseNullInput = generateSeedPhrase(null)
+let phraseNullInput2 = generateSeedPhrase(null)
+if (phraseNullInput === phraseNullInput2) {
+	fail("null seed phrases match", phraseNullInput, "\n", phraseNullInput2)
+}
+
+if (failed) {
+	process.exit(1)
+}
 
 export {}
