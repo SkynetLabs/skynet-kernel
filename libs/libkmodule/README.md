5b2013f46c2bfd1c1e33771dcdef61229fa43b53
David Vorick <david@siasky.net>
2022-05-15 20:45:34 -0400

    add connectModule support to libkmodule

--- libs/libkmodule/README.md
+++ libs/libkmodule/README.md
@@ -36,6 +36,41 @@ addHandler("someMethod", handleSomeMethod)
 onmessage = handleMessage
 ```
 
+## Error Handling
+
+libkmodule and the other Skynet core libraries depart significantly from
+idiomatic javascript in thow they handle errors. Because Skynet errors very
+frequently need to be sent over postMessage, all errors are of the form
+`string | null` as opposed to using the standard error type.
+
+Furthermore, none of the libkmodule functions have any throws. Instead, the
+functions will return errors in a tuple. When combined with the async/await
+pattern, it means a lot of the libkmodule functions have this return signature:
+`Promise<[any, string | null]>`
+
+You can interact with this code in the following manner:
+
+```ts
+async function useLibkmodule() {
+	let [value, err] = await libkmodule.someCall()
+	if (err !== null) {
+		// handle error
+		return
+	}
+
+	// There's no error, continue as normal.
+}
+```
+
+Though the pattern is unusual, it allows us to entirely avoid needing the
+try/catch pattern. This isn't the right place to talk about why try/catch is
+worth avoiding, but some of the main points include the fact that a throw can
+have objects of any type, throws aren't part of the call signature and
+therefore are often missed, programmers often avoid handling throws right away,
+and catch blocks have a completely different variable scope from try blocks.
+All of these factors make the try/catch pattern difficult to work with and
+worth avoiding.
+
 ## Seed Management
 
 If you are using `handleMessage`, you can get the seed by calling
@@ -88,12 +123,17 @@ addHandler("someMethod", handleSomeMethod)
 onmessage = handleMessage
 ```
 
-## Advanced Techniques
+## Expert Techniques
 
-We generally recommend against using these advanced techniques, as it usually
+We generally recommend against using these expert techniques, as it usually
 indicates that you are doing something wrong or that there is probably an
 easier way to achieve what you want. However, we figured we would document them
 anyway in case they proved to be necessary for certain use cases.
+
+Some wisdom: the expert feels confident in using expert techniques because they
+trust themselves not to make mistakes. The master does not feel confident in
+using expert techniques because they know that humans make mistakes at every
+level of experience.
 
 ### handleMessage Preprocessing
 
--- libs/libkmodule/src/queries.ts
+++ libs/libkmodule/src/queries.ts
@@ -8,7 +8,7 @@ import { tryStringify } from "./stringify.js"
 let queriesNonce = 0
 let queries = {} as any
 
-// Define an empty function because the linter does not like when you use '() => {}'
+// Define an empty function because the linter does not like when you use `() => {}`
 let emptyFn = function () {
 	return
 }
@@ -20,7 +20,7 @@ let emptyFn = function () {
 // support for handling queryUpdate or responseUpdate messages - they will be
 // ignored if received. If you need those messages, use 'connectModule'
 // instead.
-function callModule(module: string, method: string, data: any): Promise<[any, string | null]> {
+function callModule(module: string, method: string, data: any): Promise<[responseData: any, err: string | null]> {
 	let moduleCallData = {
 		module,
 		method,
@@ -30,6 +30,56 @@ function callModule(module: string, method: string, data: any): Promise<[any, st
 	// value is not defined, newKernelQuery will place in a no-op for us.
 	let [, query] = newKernelQuery("moduleCall", moduleCallData, emptyFn)
 	return query
+}
+
+// connectModule is a generic function to connect to a module. It is similar to
+// callModule, except that it also supports sending and receiving updates in
+// the middule of the call. If the module being called sends and update, the
+// updated will be passed to the caller through the 'receiveUpdate' function.
+// If the caller wishes to send an update to the module, it can use the
+// provided 'sendUpdate' function.
+//
+// The call signature is a bit messy, so let's disect it a bit. The input
+// values are the same as callModule, except there's a fourth input for
+// providing a 'receiveUpdate' function. It is okay to provide 'null' or
+// 'undefined' as the function to receive updates if you do not care to receive
+// or process any updates sent by the module. If you do want to receive
+// updates, the receiveUpdate function should have the following function
+// signature:
+//
+// 		`function receiveUpdate(data: any)`
+//
+// The data that gets sent is at the full discretion of the module, and will
+// depend on which method was called in the original query.
+//
+// The return value is a tuple of a 'sendUpdate' function and a promise. The
+// promise itself resolves to a tuple which matches the tuple in the
+// 'callModule' function - the first value is the response data, and the second
+// value is an error. When the promise resolves, it means the query has
+// completed and no more updates will be processed. Therefore, 'sendUpdate' is
+// only valid until the promise resolves.
+//
+// sendUpdate has the following function signature:
+//
+// 		`function sendUpdate(data: any)`
+//
+// Like 'receiveUpdate', the data that should be sent when sending an update to
+// the module is entirely determined by the module and will vary based on what
+// method was called in the original query.
+function connectModule(
+	module: string,
+	method: string,
+	data: any,
+	receiveUpdate: any
+): [sendUpdate: any, response: Promise<[responseData: any, err: string | null]>] {
+	let moduleCallData = {
+		module,
+		method,
+		data,
+	}
+	// We omit the 'receiveUpdate' function because this is a no-op. If the
+	// value is not defined, newKernelQuery will place in a no-op for us.
+	return newKernelQuery("moduleCall", moduleCallData, receiveUpdate)
 }
 
 // handleResponse will take a response and match it to the correct query.
@@ -58,11 +108,22 @@ function handleResponse(event: MessageEvent) {
 	delete queries[event.data.nonce]
 }
 
-// handleResponseUpdate currently discards all responseUpdates.
+// handleResponseUpdate attempts to find the corresponding query using the
+// nonce and then calls the corresponding receiveUpdate function.
 //
-// TODO: Implement this.
+// Because response and responseUpdate messages are sent asynchronously, it's
+// completely possible that a responseUpdate is received after the query has
+// been closed out by a response. We therefore just ignore any messages that
+// can't be matched to a nonce.
 function handleResponseUpdate(event: MessageEvent) {
-	return event
+	// Ignore this message if there is no corresponding query, the query may
+	// have been closed out and this message was just processed late.
+	if (!(event.data.nonce in queries)) {
+		return
+	}
+
+	// Pass the update along to the corresponding receiveUpdate function.
+	queries[event.data.nonce].receiveUpdate(event.data.data, event.data.err)
 }
 
 // handleQueryUpdate currently discards all queryUpdates.
@@ -82,12 +143,20 @@ function handleQueryUpdate(event: MessageEvent) {
 //
 // NOTE: Typically developers should not use this function. Instead use
 // 'callModule' or 'connectModule'.
-function newKernelQuery(method: any, data: any, receiveUpdate: any): [any, Promise<[any, string | null]>] {
+function newKernelQuery(
+	method: any,
+	data: any,
+	receiveUpdate: any
+): [sendUpdate: any, response: Promise<[responseData: any, err: string | null]>] {
 	let nonce = queriesNonce
 	queriesNonce += 1
-	let sendUpdate = function (updateData: any) {
-		// TODO: Implement
-		return updateData
+	let sendUpdate = function (updateData: any, updateErr: string | null) {
+		postMessage({
+			method: "responseUpdate",
+			nonce,
+			err: updateErr,
+			data: updateData,
+		})
 	}
 
 	// Check that receiveUpdate is set.
@@ -108,4 +177,4 @@ function newKernelQuery(method: any, data: any, receiveUpdate: any): [any, Promi
 	return [sendUpdate, p as any]
 }
 
-export { callModule, handleQueryUpdate, handleResponse, handleResponseUpdate, newKernelQuery }
+export { callModule, connectModule, handleQueryUpdate, handleResponse, handleResponseUpdate, newKernelQuery }
