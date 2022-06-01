1f1233ea0db09c587f517b242e70001052789e2c
David Vorick <david@siasky.net>
2022-06-01 12:19:47 -0400

    add 404 page to halt gatsby error

---
+++ webapps/kernel-test-suite2/src/pages/404.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+// Establish the 404 page.
+const Page404 = () => {
+	return (
+		<main>
+			<title>Libkernel Test Suite</title>
+			<h1>404</h1>
+		</main>
+	)
+}
+
+export default Page404
