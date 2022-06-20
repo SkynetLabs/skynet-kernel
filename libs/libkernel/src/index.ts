36006669799ab9d7026b3c5164a509f61d4dee1e
David Vorick <david@siasky.net>
2022-06-19 23:49:20 -0400

    export kernelLoaded in libkernel

--- libs/libkernel/src/index.ts
+++ libs/libkernel/src/index.ts
@@ -1,4 +1,4 @@
-export { loginComplete, logoutComplete, openAuthWindow } from "./auth.js"
+export { kernelLoaded, loginComplete, logoutComplete, openAuthWindow } from "./auth.js"
 export { download } from "./messagedownload.js"
 export { registryRead, registryWrite } from "./messageregistry.js"
 export { upload } from "./messageupload.js"
