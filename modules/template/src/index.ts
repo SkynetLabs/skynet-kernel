af4925a23c798e101b1e2cb8bfb587a3b10a48e2
David Vorick <david@siasky.net>
2022-07-07 12:53:41 -0400

    update template code

--- modules/template/src/index.ts
+++ modules/template/src/index.ts
@@ -6,13 +6,13 @@ onmessage = handleMessage;
 
 // handleMethodName will handle a call to methodName.
 function handleMethodName(aq: activeQuery) {
-  // Check the inputs
-  // Here, expecting data to have a "message" field of string
+  // We are expecting there to be one input field
+  // 'message' which has a string value.
   if (typeof aq.callerInput.message !== "string") {
     aq.reject("Field `message` expected to be a string");
     return;
   }
 
   // Respond to query with data
-  aq.respond({ key: "String Value" });
+  aq.respond({ methodResponse: "Your Message:" + aq.callerInput.message });
 }
