d20cd45bea34f1b179365fc4ed39a00f5273ecf1
David Vorick <david@siasky.net>
2022-07-07 05:18:08 -0400

    introduce the crypto libraries and some helper files

---
+++ libs/libskynet/src/index.ts
@@ -0,0 +1,5 @@
+export { BLAKE2B_HASH_SIZE, blake2b } from "./blake2b.js";
+export { Ed25519Keypair, ed25519KeypairFromEntropy, ed25519Sign, ed25519Verify } from "./ed25519.js";
+export { addContextToErr, composeErr } from "./err.js";
+export { SHA512_HASH_SIZE, sha512 } from "./sha512.js";
+export { tryStringify } from "./stringifytry.js";
