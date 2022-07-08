56f5aba7fa23507e1eaafffae58de73adea520cf
David Vorick <david@siasky.net>
2022-07-08 10:45:30 -0400

    address comments

--- libs/libskynet/src/index.ts
+++ libs/libskynet/src/index.ts
@@ -1,5 +1,4 @@
-export { BLAKE2B_HASH_SIZE, blake2b } from "./blake2b.js";
-export { Ed25519Keypair, ed25519KeypairFromEntropy, ed25519Sign, ed25519Verify } from "./ed25519.js";
-export { addContextToErr, composeErr } from "./err.js";
-export { SHA512_HASH_SIZE, sha512 } from "./sha512.js";
-export { tryStringify } from "./stringifytry.js";
+export { Ed25519Keypair, ed25519KeypairFromEntropy, ed25519Sign, ed25519Verify } from "./ed25519";
+export { addContextToErr } from "./err";
+export { SHA512_HASH_SIZE, sha512 } from "./sha512";
+export { objAsString } from "./objAsString";
