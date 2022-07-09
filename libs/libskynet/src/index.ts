56de42f44b609b70a9ebd3379798b2aa82b1547c
David Vorick <david@siasky.net>
2022-07-08 22:08:17 -0400

    go back to using .js suffixes

--- libs/libskynet/src/index.ts
+++ libs/libskynet/src/index.ts
@@ -1,4 +1,4 @@
-export { Ed25519Keypair, ed25519KeypairFromEntropy, ed25519Sign, ed25519Verify } from "./ed25519";
-export { addContextToErr } from "./err";
-export { SHA512_HASH_SIZE, sha512 } from "./sha512";
-export { objAsString } from "./objAsString";
+export { Ed25519Keypair, ed25519KeypairFromEntropy, ed25519Sign, ed25519Verify } from "./ed25519.js";
+export { addContextToErr } from "./err.js";
+export { SHA512_HASH_SIZE, sha512 } from "./sha512.js";
+export { objAsString } from "./objAsString.js";
