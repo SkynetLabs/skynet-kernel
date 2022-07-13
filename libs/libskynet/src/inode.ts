82922ed9e38e1bae931925ac8b7b268e3062de85
David Vorick <david@siasky.net>
2022-07-12 23:14:40 -0400

    update namespaceInode

--- libs/libskynet/src/inode.ts
+++ libs/libskynet/src/inode.ts
@@ -1,4 +1,6 @@
+import { bufToB64 } from "./encoding.js"
 import { error } from "./types.js"
+import { sha512 } from "./sha512.js"
 
 // namespaceInode is a function for namespacing inodes based on the type of
 // file that is being used, this way a file that is created using
@@ -6,16 +8,28 @@ import { error } from "./types.js"
 // 'openIndependentFile' - it can only be opened by 'openIndependentFileSmall'
 // as the two functions with the same inode will actually reference different
 // files on Skynet.
-//
-// No cryptography is needed here, the inodes don't get exposed publicly.
 function namespaceInode(filetype: string, inode: string): [string, error] {
+	// We pad out the filetype to 32 characters to ensure that two different
+	// filetypes can never have a filetype+inode combination that will collide.
+	// If the filetype is different, the final result will definitely also be
+	// different.
 	if (filetype.length > 32) {
 		return ["", "filetype can be at most 32 characters"]
 	}
 	while (filetype.length < 32) {
-		filetype += " "
+		filetype += "_"
 	}
-	return [filetype + inode, null]
+	// Add the inode to the extended filetype.
+	filetype += inode
+
+	// We hash the result to make it smaller. Because we use this as an
+	// encryption key and not for authentication, our security model only
+	// requires 16 bits of entropy. We therefore only take the first 16 bytes
+	// of the hash and return the base64 encoded string.
+	const fullHash = sha512(new TextEncoder().encode(filetype + inode))
+	const quarterHash = fullHash.slice(0, 16)
+	const b64 = bufToB64(quarterHash)
+	return [b64, null]
 }
 
 export { namespaceInode }
