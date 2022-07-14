2cbd72fc1b2bf08a71dca4725e8202ec1ea1002d
Michał Leszczyk <michasko37@gmail.com>
2022-07-14 07:32:06 +0200

    Use jest-ts-webcompat-resolver to make .js extensions work in tests

--- libs/libskynet/jest.config.js
+++ libs/libskynet/jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
   preset: "ts-jest",
+  resolver: "jest-ts-webcompat-resolver",
   testEnvironment: "node",
   testPathIgnorePatterns: [
     "src-test/test.ts", // Don't run legacy tests with Jest
