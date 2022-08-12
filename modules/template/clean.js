63674addfe160dc0b5cce6d07de77ea411784f96
David Vorick <david@skynetlabs.com>
2022-08-12 09:43:09 -0400

    update template module

---
+++ modules/template/clean.js
@@ -0,0 +1,5 @@
+import * as fs from "fs";
+fs.rmSync("build", { recursive: true, force: true });
+fs.rmSync("dist", { recursive: true, force: true });
+fs.rmSync("dist-build", { recursive: true, force: true });
+fs.rmSync("node_modules", { recursive: true, force: true });
